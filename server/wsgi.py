0bdd69ae01d3fee5efa292ce4e5883ed8583643e
KazooTTT <yjwang.echo@gmail.com>
2024-01-03 00:24:11 +0800

    chore:允许所有域

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,7 +8,7 @@ from flask import Flask, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(
-    app, cors_allowed_origins='http://localhost:3000', sync_mode='eventlet')
+    app, cors_allowed_origins='*', sync_mode='eventlet')
 
 
 @app.route('/')
